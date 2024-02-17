1b5d881f15ded80938ff784f7e32e198bcf11773
ColexGod <ccfelix2004@gmail.com>
2024-02-17 15:34:02 -0500

    changes

---
+++ js/langue.js
@@ -0,0 +1,20 @@
+/*
+<script>
+function downloadCV() {
+    var documentUrl = '../cv.pdf'; 
+    
+    var anchorElement = document.createElement('a');
+    
+    anchorElement.href = documentUrl;
+    
+    anchorElement.download = 'cv.pdf';
+    
+    document.body.appendChild(anchorElement);
+    
+    anchorElement.click();
+    
+    document.body.removeChild(anchorElement);
+}
+</script>
+
+*/
